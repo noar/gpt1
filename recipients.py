36a8663447baf6d7ae7c294cb6abadea8d564e47
noar <spadrolelavie@gmail.com>
2023-03-27 21:23:18 +0200

    let recipients.py use key fingerprint in lieu of key id

--- recipients.py
+++ recipients.py
@@ -1,44 +1,54 @@
+import os
 import json
-import os.path
-import gnupg
+import subprocess
 
 class Recipient:
-    def __init__(self, name, email, fingerprint):
+    def __init__(self, name, email, fingerprint, ip=None, port=None):
         self.name = name
         self.email = email
         self.fingerprint = fingerprint
+        self.ip = ip
+        self.port = port
 
-class RecipientList:
-    def __init__(self, filepath):
-        self.filepath = filepath
-        self.recipients = []
-        self.gpg = gnupg.GPG()
+    def __str__(self):
+        return f"{self.name} <{self.email}> ({self.fingerprint})"
 
-    def add(self, recipient):
-        self.recipients.append(recipient)
+    def get_id(self):
+        return self.fingerprint.replace(" ", "")[-8:]
 
-    def remove(self, index):
-        self.recipients.pop(index)
+    @staticmethod
+    def from_json(json_str):
+        data = json.loads(json_str)
+        return Recipient(data["name"], data["email"], data["fingerprint"], data.get("ip"), data.get("port"))
 
-    def get(self, index):
-        return self.recipients[index]
+    def to_json(self):
+        data = {"name": self.name, "email": self.email, "fingerprint": self.fingerprint}
+        if self.ip:
+            data["ip"] = self.ip
+        if self.port:
+            data["port"] = self.port
+        return json.dumps(data)
 
-    def count(self):
-        return len(self.recipients)
+    @staticmethod
+    def list_from_file(filename):
+        if not os.path.exists(filename):
+            return []
 
-    def load(self):
-        if os.path.isfile(self.filepath):
-            with open(self.filepath, 'r') as f:
-                data = f.read()
-                if data:
-                    decrypted_data = str(self.gpg.decrypt(data))
-                    self.recipients = [Recipient(**r) for r in json.loads(decrypted_data)]
+        with open(filename) as f:
+            return [Recipient.from_json(line.strip()) for line in f if line.strip()]
 
-    def save(self):
-        with open(self.filepath, 'w') as f:
-            data = json.dumps([r.__dict__ for r in self.recipients])
-            encrypted_data = str(self.gpg.encrypt(data, self.gpg.list_keys()[0]['fingerprint']))
-            f.write(encrypted_data)
+    @staticmethod
+    def list_to_file(filename, recipients):
+        with open(filename, "w") as f:
+            for recipient in recipients:
+                f.write(recipient.to_json() + "\n")
 
-    def get_fingerprints(self):
-        return [r.fingerprint for r in self.recipients]
+    @staticmethod
+    def get_fingerprint(email):
+        try:
+            output = subprocess.check_output(f"gpg --list-keys {email}", shell=True, stderr=subprocess.STDOUT)
+            lines = output.decode().split("\n")
+            fingerprint_line = next(line for line in lines if "Key fingerprint" in line)
+            return fingerprint_line.split("=")[1].strip()
+        except subprocess.CalledProcessError as e:
+            return None
